c9f9def1571caad06bd03f0ea90e5286a4273e4d
Drue Gilbert <drue.gilbert@gmail.com>
2019-07-03 16:36:01 -0700

    started adding routing

--- src/App.js
+++ src/App.js
@@ -1,93 +1,31 @@
 import React from 'react';
-import Table from './components/Table';
+import 'react-router';
+import {BrowserRouter, Route, Link, Redirect} from 'react-router-dom';
+import TableContainer from './components/TableContainer';
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			newCol: '',
-			columns: [{ id: 'col0avg', name: '[Average Score]', type: 1, sort: '▽' }],
-			numRows: 0
-		};
-		this.handleColInputChange = this.handleColInputChange.bind(this);
-		this.handleNewCol = this.handleNewCol.bind(this);
-		this.handleDelCol = this.handleDelCol.bind(this);
-		this.handleNewRow = this.handleNewRow.bind(this);
-		this.handleDelRow = this.handleDelRow.bind(this);
-	}
-
-	handleColInputChange(event) {
-		this.setState({ newCol: event.target.value });
-	}
-
-	handleNewCol(event) {
-		let text = this.state.newCol.trim();
-		if (text.length > 0) {
-			let cols = this.state.columns.map(x => x);
-			let avg = cols.pop(); // colAvg is always last
-			let type = 't';
-			if (event.target.name === '+') type = 1;
-			else if (event.target.name === '-') type = -1;
-			cols.push({ id: 'col' + cols.length, name: text, type: type, sort: '' });
-			avg.id = 'col' + cols.length + 'avg';
-			cols.push(avg);
-			this.setState({ columns: cols, newCol: '' });
-		}
-	}
-
-	handleDelCol() {
-		if (this.state.columns.length > 1) {
-			let cols = this.state.columns.map(x => x);
-			let avg = cols.pop(); // colAvg is always last
-			cols.pop();
-			avg.id = 'col' + cols.length + 'avg';
-			cols.push(avg);
-			this.setState({ columns: cols });
-		}
-	}
-
-	handleNewRow() {
-		const num = this.state.numRows + 1;
-		this.setState({ numRows: num });
-	}
-
-	handleDelRow() {
-		if (this.state.numRows > 0) {
-			const num = this.state.numRows - 1;
-			this.setState({ numRows: num });
-		}
-	}
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	//
+	// 	};
+	// }
 
 	render() {
 		return (
-			<div className="container-fluid">
-				<h1 className="text-center">Data Comparison Spreadsheet Tool</h1>
-				<div className="input-group mb-2">
-					<input className="form-control" type="text" placeholder="New Column Name" value={this.state.newCol}
-						onChange={this.handleColInputChange}/>
-					<div className="input-group-append btn-group">
-						<button className="btn btn-outline-success" name="t" onClick={this.handleNewCol}>Text</button>
-						<button className="btn btn-outline-success" name="+" onClick={this.handleNewCol}>Number ▼</button>
-						<button className="btn btn-outline-success" name="-" onClick={this.handleNewCol}>Number ▲</button>
+			<BrowserRouter>
+				<div className="container-fluid">
+					<div className="row px-5">
+						<div className="col-8">
+							<h1>Data Comparison Spreadsheet Tool</h1>
+						</div>
+						<div className="col-4">
+							<Link to="/new" className="btn btn-primary">New</Link>
+						</div>
 					</div>
+					<Route path="/new" component={TableContainer}/>
 				</div>
-				<Table key="table" columns={this.state.columns} numRows={this.state.numRows}/>
-				<div className="btn-group mt-2 mr-2">
-					<button className="btn btn-outline-success" onClick={this.handleNewRow}>Add New Row</button>
-					<button className="btn btn-outline-danger" onClick={this.handleDelRow}>Delete Bottom Row</button>
-				</div>
-				<button className="btn btn-outline-danger mt-2" onClick={this.handleDelCol}>Delete Last Column</button>
-				<p className="mt-2">Double-click cells to edit.&ensp;Press 'Enter' to submit changes.</p>
-				<hr/>
-				<p className="text-justify">The <span className="my-1 btn btn-sm btn-outline-success">Text</span> button creates
-				a new text column.&ensp;The <span className="my-1 btn btn-sm btn-outline-success">Number ▼</span> button creates
-				a number column that scores larger numbers higher.&ensp;The&nbsp;
-				<span className="my-1 btn btn-sm btn-outline-success">Number ▲</span> button creates a number column that scores
-				smaller numbers higher.&ensp;Scores are between 1 and 10, and are calculated by percent rank out of the entire
-				column.&ensp;Column scores are recalculated every time a cell is edited.&ensp;Click column headers to sort.
-				&ensp;Sort by <span className="text-primary">[Average Score]</span> descending to see the rows in order of best
-				overall.</p>
-			</div>
+			</BrowserRouter>
 		);
 	}
 }
