7da10e252038b8bca89dd9edcf2d9925f038b048
Drue Gilbert <drue.gilbert@gmail.com>
2019-07-03 18:13:48 -0700

    added home & new routes, plus welcome message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'react-router';
 import {BrowserRouter, Route, Link, Redirect} from 'react-router-dom';
+import Welcome from './components/Welcome';
 import TableContainer from './components/TableContainer';
 
 export default class App extends React.Component {
@@ -12,17 +13,22 @@ export default class App extends React.Component {
 	// }
 
 	render() {
+		// <Link to="/load" className="btn btn-secondary"><strong>Load</strong></Link>
 		return (
 			<BrowserRouter>
 				<div className="container-fluid">
-					<div className="row px-5">
-						<div className="col-8">
-							<h1>Data Comparison Spreadsheet Tool</h1>
+					<div className="row px-3 mb-2 border-bottom border-primary">
+						<div className="col-lg-8 col-md-12 pt-1">
+							<h2 className="text-primary">Data Comparison Spreadsheet Tool</h2>
 						</div>
-						<div className="col-4">
-							<Link to="/new" className="btn btn-primary">New</Link>
+						<div className="col-lg-4 col-md-12 pt-lg-2 pb-md-2">
+							<div className="btn-group">
+								<Link to="/" className="btn btn-primary"><strong>Home</strong></Link>
+								<Link to="/new" className="btn btn-success"><strong>New</strong></Link>
+							</div>
 						</div>
 					</div>
+					<Route exact path="/" component={Welcome}/>
 					<Route path="/new" component={TableContainer}/>
 				</div>
 			</BrowserRouter>
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import dcst from './dcst.png';
+
+export default class Welcome extends React.Component {
+	render() {
+		return (
+			<div className="text-center">
+				<h1 className="display-3">Welcome to DCST</h1>
+			</div>
+		);
+	}
+}
